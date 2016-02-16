b15c7a7ec52d4ced0ccaedca5bc4f50733ea2b43
Winfried <wklinker@2orgu.com>
2016-02-16 11:50:42 +0100

    Updated hook script to use ConfigParser from Cordova-Common instead of Cordova-lib (with Fallback)

--- scripts/appinsights_before_prepare.js
+++ scripts/appinsights_before_prepare.js
@@ -7,19 +7,27 @@ Licensed under the Apache License, Version 2.0.  See License.txt in the project 
 
 module.exports = function (ctx) {
 
-    var ConfigParser = ctx.requireCordovaModule('../configparser/ConfigParser');
+       var ConfigParser;
     var path = ctx.requireCordovaModule('path');
     var shell = ctx.requireCordovaModule('shelljs');
+
+ // If we are running Cordova 5.4 or abova - use parser from cordova-common.
+     // Otherwise - from cordova-lib.+
+    try {
+       ConfigParser = ctx.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+     } catch (e) {
+       ConfigParser = ctx.requireCordovaModule('cordova-lib/src/configparser/ConfigParser')
+     }
 
     var pluginConfigFile = path.resolve(ctx.opts.plugin.dir, 'www', 'AppInsights.js');
     var projectConfigXml = new ConfigParser(path.join(ctx.opts.projectRoot, 'config.xml'));
     var instrKey = projectConfigXml.getGlobalPreference('instrumentation_key');
 
-    console.log("Replacing 'instrumentationKey' parameter in plugin");
+    //console.log("Replacing 'instrumentationKey' parameter in plugin with " + instrKey);
 
     if (instrKey) {
         // replace instrumentation key stub with provided value
-        console.log("Replacing 'instrumentationKey' parameter in plugin");
+        console.log("Replacing 'instrumentationKey' parameter in plugin with value " + instrKey);
         shell.sed('-i',
             /instrumentationKey:\s"(.*?)"/g,
             'instrumentationKey: "' + instrKey + '"',
